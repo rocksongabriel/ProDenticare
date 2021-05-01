d21162006e3517dc20a3c3ec492eb470ff739156
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-01 11:42:37 +0000

    Added the hero image on the homepage

--- home/models.py
+++ home/models.py
@@ -1,7 +1,53 @@
 from django.db import models
-
+from wagtail.admin.edit_handlers import (FieldPanel, MultiFieldPanel,
+                                         PageChooserPanel)
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    pass
+    """Page model for the homepage"""
+    # todo 
+    # 1. add banner
+    # 2. add text on banner
+    # 3. a button to an action page on banner
+    # 4. list of services
+    # 5. testimonials - slider
+    # 6. small about on homepage
+    max_count = 1
+
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        related_name="+",
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        help_text="Pick an image that will displayed as the banner image"
+    )
+    banner_text = models.CharField(
+        max_length=200,
+        help_text="Enter text that will be displayed on the banner image",
+        default="",
+    )
+    banner_button_text = models.CharField(
+        max_length=20,
+        help_text="Enter text that will appear on the button on the banner",
+        default="",
+    )
+    banner_button_page = models.ForeignKey(
+        "wagtailcore.Page",
+        on_delete=models.SET_NULL,
+        related_name="+",
+        null=True,
+        blank=True,
+        help_text="Select a page that this button leads to"
+    )
+
+    
+    content_panels = Page.content_panels + [
+        ImageChooserPanel("banner_image"),
+        FieldPanel("banner_text"),
+        FieldPanel("banner_button_text"),
+        PageChooserPanel("banner_button_page"),
+    ]
+
