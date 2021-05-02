75ca827b0cf64a9f9ff18a51d7964d59d2e76159
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-02 17:16:27 +0000

    added a streams app to keep my custom blocks in

---
+++ streams/blocks.py
@@ -0,0 +1,9 @@
+from wagtail.core import blocks
+
+
+class TitleAndTextBlock(blocks.StructBlock):
+    title = blocks.CharBlock(max_length=50, help_text="Enter title"),
+    text = blocks.RichTextBlock()
+
+    class Meta:
+        template = "streams/title_and_text_block.html"
